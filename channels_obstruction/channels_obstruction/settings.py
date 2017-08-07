13dd6843221f1da6e7196a78919a567a005cc5a8
johnedstone <johnedstone@gmail.com>
2017-08-07 02:08:13 +0000

    parts one and two complete

---
+++ channels_obstruction/channels_obstruction/settings.py
@@ -0,0 +1,105 @@
+import os
+ 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!9d55@b7@+$&!oqm28j2re@5x7ttd6-g2hkchg$b#-_te^esoq'
+ 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ 
+ALLOWED_HOSTS = ['*']
+ 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'game',
+    'channels',
+]
+ 
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+ 
+ROOT_URLCONF = 'channels_obstruction.urls'
+ 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+ 
+WSGI_APPLICATION = 'channels_obstruction.wsgi.application'
+ 
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+ 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+ 
+ 
+LOGIN_REDIRECT_URL = '/lobby/'
+LOGIN_URL = '/login/'
+ 
+LANGUAGE_CODE = 'en-us'
+ 
+TIME_ZONE = 'UTC'
+ 
+USE_I18N = True
+ 
+USE_L10N = True
+ 
+USE_TZ = True
+ 
+ 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+ 
+ 
+CHANNEL_LAYERS = {
+ "default": {
+ "BACKEND": "asgiref.inmemory.ChannelLayer",
+ "ROUTING": "channels_obstruction.routing.channel_routing",
+ },
+}
